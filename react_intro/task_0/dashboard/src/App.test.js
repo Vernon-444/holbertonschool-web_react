0e22a6e90c5ce9eb17e8f090b2cfe25850823026
Brayden Vernon <mt.vernon44@gmail.com>
2023-09-06 12:30:19 -0500

    task 6, prep for 7

--- react_intro/task_0/dashboard/src/App.test.js
+++ react_intro/task_0/dashboard/src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  it('Tests that App renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('Tests that App renders a div with class App-header', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App-header').exists()).toBe(true);
+  });
+
+  it('Tests that App renders a div with class App-body', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App-body').exists()).toBe(true);
+  });
+
+  it('Tests that App renders a div with class App-footer', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App-footer').exists()).toBe(true);
+  });
 });
